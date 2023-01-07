567335220713955b4e2156a68aa0d8b5d94618f1
mohit03nandan <mohitnandan81825@gmail.com>
2023-01-07 22:11:48 +0530

    wrapup

--- src/Components/Page/Home.js
+++ src/Components/Page/Home.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
-import Form from './Form'
-import Card from './Card'
+import Form from '../Form/Form'
+import Card from '../Card/Card'
 const Home = () => {
 
     const [CardValue, setCardValue] = useState('');
